9ece33f0886554451c9d51835f6260b0c1aa5880
Mel Tajon <mel.tajon@kwallcompany.com>
2018-02-04 17:06:13 -0800

    consolidated AJAX to /js/load-json.js

---
+++ js/load-json.js
@@ -0,0 +1,33 @@
+/**
+ * Code by "Learn Web Code"
+ * https://www.youtube.com/watch?v=wSNa5b1mS5Y
+ */
+
+function createHTML(data) {
+  var rawTemplate = document.getElementById("template").innerHTML;
+  var compileTemplate = Handlebars.compile(rawTemplate);
+  var generatedTemplate = compileTemplate(data);
+  
+  var postsContainer = document.getElementById("ajax");
+  postsContainer.innerHTML = generatedTemplate;
+}
+
+// var myRequest = JSON.parse(document.getElementById('restaurants').innerHTML);
+
+var myRequest = new XMLHttpRequest();
+myRequest.open('GET', 'restaurants.json');
+
+myRequest.onload = function() {
+  if (myRequest.status >= 200 && myRequest.status < 400) {
+    var data = JSON.parse(myRequest.responseText);
+    createHTML(data);
+  } else {
+    console.log("Connected established, but with error.");
+  }
+};
+
+myRequest.onerror = function() {
+  console.log("Connection Error.");
+};
+
+myRequest.send();
